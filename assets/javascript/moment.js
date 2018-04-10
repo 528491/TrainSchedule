b3ddd2821b14e22fafd28b73b056128f998a83f7
528491 <jacobminasian@Jacobs-MacBook-Pro.local>
2018-04-09 23:35:44 -0700

    Initialized firebase

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -1,5 +1,38 @@
-
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAeHBKQarrrBJtsTeE43EhgWVFQut9wtK0",
+    authDomain: "trainschedule-eac12.firebaseapp.com",
+    databaseURL: "https://trainschedule-eac12.firebaseio.com",
+    projectId: "trainschedule-eac12",
+    storageBucket: "",
+    messagingSenderId: "937485885457"
+  };
+  firebase.initializeApp(config);
 
 $(document).ready(function(){
+    //Create the variables that will hold the information submitted from each form.
+    var trainName = "";
+    var destination = "";
+    var frequency = 0;
+    var firstTrainTime = "";
+    var minutesAway = 0;
+
+    //Create a click listener for the addTrain button that will update the necessary variables
+    //in accordance with the user input.
+    $("#addTrain").on("click", function(){
+        trainName = $("#trainNameInput").val().trim();
+        destination = $("#trainDestinationInput").val().trim();
+        frequency = $("#trainTimeInput").val().trim();
+        firstTrainTime = $("#trainFrequencyInput").val().trim();
+
+        firebase.database().ref().set({
+            trainName: trainName,
+            destination: destination,
+            frequency: frequency,
+            firstTrainTime: firstTrainTime
+        });
+    });
+
+
 
 });
